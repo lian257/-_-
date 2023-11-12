6a93e5beb3208ca663a188462c9937d44b355dec
tlian <tlian2528@gmail.com>
2023-11-12 09:29:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/css/global.css'
+import '@/assets/css/iconfont/iconfont.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import request  from "@/utils/request";
+
+Vue.use(ElementUI, { size: 'small' });
+
 Vue.config.productionTip = false
+
+Vue.prototype.$request = request
 
 new Vue({
   router,
